8c8b4addec2241458dc4e852d1e165da98b65d0d
Lehersun <kpekep@list.ru>
2020-10-04 23:03:01 +0300

    lesson3

--- js.js
+++ js.js
@@ -1,127 +1,43 @@
-function task1(firstVal, secondVal) {
-  let result;
-  if (firstVal == secondVal) {
-    result = "они равны!";
-  } else if (firstVal < secondVal) {
-    result = "первое число меньше второго!";
-  } else {
-    result = "первое число больше второго!";
-  }
-  return result;
-}
-
-function task2(firstVal, secondVal) {
-  let result;
-  let years = [];
-  result = 0;
-  for (let i = firstVal; i < secondVal; i++) {
-    if (i % 4 == 0) {
-      if (i % 100 !== 0) {
-        years.push(i);
-        result++;
-      } else if (i % 400 == 0) {
-        years.push(i);
-        result++;
-      }
-    }
-  }
-  console.log(years);
-  return result;
-}
-// Задача №1
+// Задача
 document.querySelector("#button1").addEventListener("click", (e) => {
-  let popitki = 10;
-  do {
-    let val1 = prompt("Введите первое число(у Вас осталось " + popitki + " попыток)");
 
-    if (val1 == null) {
-      alert("Прощай");
-      break;
-    } else if (isNaN(val1)) {
-      alert("Это не число, попробуйте снова");
-    } else if (!val1.trim()) {
-      alert("Строка не может быть пустой");
-    } else {
-      do {
-        let val2 = prompt("Введите второе число(у Вас осталось " + popitki + " попыток)");
-        if (val2 == null) {
-          alert("Прощай");
-          break;
-        } else if (isNaN(val2)) {
-          alert("Это не число, попробуйте снова");
-        } else if (!val2.trim()) {
-          alert("Строка не может быть пустой");
-        } else {
-          let res = task1(val1, val2);
-          alert("Вы ввели " + val1.trim() + " и " + val2.trim() + " и однозначно " + res);
-          break;
-        }
-        popitki--;
-      }
-      while (popitki > 0);
-      break;
-    }
-    popitki--;
-  } while (popitki > 0);
-});
-
-
-// Задача №2
-
-document.querySelector("#button2").addEventListener("click", (e) => {
-  let popitki = 10;
-  do {
-    let val1 = prompt("Начнем вычислять високосные года. Введите год с когорого начинаем(у Вас осталось " + popitki + " попыток)");
-
-    if (val1 == null) {
-      alert("Прощай");
-      break;
-    } else if (isNaN(val1)) {
-      alert("Это вообще не год или Вы словами решили написать?");
-    } else if (!val1.trim()) {
-      alert("Строка не может быть пустой");
-    } else {
-      do {
-        let val2 = prompt("Введите год до которого будем считать(у Вас осталось " + popitki + " попыток)");
-        if (val2 == null) {
-          alert("Прощай");
-          break;
-        } else if (isNaN(val2)) {
-          alert("Это вообще не год или Вы словами решили написать?");
-        } else if (!val2.trim()) {
-          alert("Строка не может быть пустой");
-        } else if (val1 > val2) {
-          alert("Второй год не может быть больше первого, попробуй сначала");
-          break;
-        } else {
-          let res = task2(val1, val2);
-          alert("Между " + val1.trim() + " и " + val2.trim() + " " + res + " високосных и все они выведены в консоль");
-          break;
-        }
-        popitki--;
-      }
-      while (popitki > 0);
-      break;
-    }
-    popitki--;
-  } while (popitki > 0);
-});
-
-// Задача №3
-
-document.querySelector("#button3").addEventListener("click", (e) => {
-  let summa = 0;
-  while (true) {
-    let val = prompt("Уважаемый пользовать, введите что угодно, но лучше число");
-    if (val !== null) {
-      if (val.trim() !== "") {
-        if (!isNaN(val)) {
-          summa = summa + parseInt(val);
-        }
-      }
-    } else {
-      alert("Сумма всех введенных чисел равна " + summa);
-      break;
-    }
+  function generateRandomDigit() {
+    let digit = Math.floor(Math.random() * 100);
+    return digit;
   }
+
+  let popitki = 10;
+  let i = generateRandomDigit();
+
+
+  do {
+    let val = prompt("Я загадал число от 0 до 100. Отгадаете?(у Вас осталось " + popitki + " попыток)");
+    console.log(i);
+
+    if (val == null) {
+      alert("Хорошо поиграли");
+      break;
+    } else if (isNaN(val)) {
+      alert("Это не число, попробуйте снова");
+    } else if (!val.trim()) {
+      alert("Строка не может быть пустой");
+    } else {
+      if (parseInt(val) < i) {
+        alert("Больше!");
+      } else if (parseInt(val) > i) {
+        alert("Меньше!");
+      } else {
+        alert("Как ты догадался, что я загадал " + i + "?");
+        let newGame = confirm("Сыграем еще разок?");
+        if (newGame == true) {
+          i = generateRandomDigit();
+          popitki = 11;
+        } else {
+          break
+        }
+      }
+    }
+    popitki--;
+  } while (popitki > 0);
+  alert("Хорошо поиграли");
 });
